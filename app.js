954bc8ca72bcb6cc845bb79ac811e5904ef7315c
VictorKmin <kminvictor@gmail.com>
2018-11-13 15:58:38 +0200

    Start writing single item parser

--- app.js
+++ app.js
@@ -8,29 +8,57 @@ let arrOfDivs = []
 async function rozetkaParser(url) {
     let mainHtmlFile = await request(url)
 
-
+    // console.log(mainHtmlFile);
     // _____________________________________________________
     // _____________________________________________________
-    // _____________________________________________________
 
-    let $ = await cheerio.load(mainHtmlFile)
-    let arr = $('div.g-i-search-list')[0].children;
+    //TODO NORMALLY LINK OF SINGLE PHONE
+    // let $ = await cheerio.load(mainHtmlFile)
+    // let arr = $('div.g-i-search-list')[0].children;
+    //
+    //
+    // $(arr).each((i, elem) => {
+    //     let n = ($(elem).find('.g-i-list-title').html())
+    //
+    //     if (n !== null){
+    //         arrOfDivs.push(n)
+    //         console.log('_______________');
+    //         console.log($(n).attr('href'));
+    //         console.log('_______________');
+    //     }
+    // })
 
+    arrOfDivs.push("https://rozetka.com.ua/ua/samsung_sm_n950fzvdsek/p21617366/")
+    arrOfDivs.push("https://rozetka.com.ua/ua/samsung_sm_n960fzbhsek/p57888057/")
+    arrOfDivs.push("https://rozetka.com.ua/ua/samsung_sm_n960fzkdsek/p48750886/")
 
-    $(arr).each((i, elem) => {
-        let n = ($(elem).find('.g-i-list-title').html())
-
-        // TODO NORMAL REGEXP
-        if (n !== null){
-            arrOfDivs.push(n)
-            console.log('_______________');
-            console.log(n.match('/(https?:\\/\\/[^\\s]+)/g;'));
-            console.log('_______________');
-        }
-
-    })
-    console.log(arrOfDivs);
+    await getCharacteristic(arrOfDivs)
 }
+
+
+function getCharacteristic(phoneUrls) {
+    let htmlFilesWithPhoneCharacteristic = [];
+    phoneUrls.forEach(phoneIrl => {
+        phoneIrl = phoneIrl + '#tab=characteristics';
+        request(phoneIrl).then(htmlWithPhoneCharacteristic => {
+            htmlFilesWithPhoneCharacteristic.push(htmlWithPhoneCharacteristic)
+        })
+    });
+    console.log();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 rozetkaParser('https://rozetka.com.ua/ua/search/?class=0&text=samsung++note&section_id=80003')
 
