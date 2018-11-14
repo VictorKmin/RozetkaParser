07d39a5f245b9f2116dcd2d706a6910a1ea4f182
VictorKmin <kminvictor@gmail.com>
2018-11-14 17:11:36 +0200

    Finished! Refactor and remove some things

--- app.js
+++ app.js
@@ -1,57 +1,55 @@
 const cheerio = require('cheerio');
 const request = require('request-promise');
 
-
 /**
  * @param url - URL of page what we will be parse
  * This is main method of parser.
  */
 async function rozetkaParser(url) {
-    let arrayOfPhoneLinks = [];
-
     let mainHtmlFile = await request(url);
-
     let $ = await cheerio.load(mainHtmlFile);
+    //Take array of phones
     let arr = $('.g-i-search-list')[0].children;
 
+    let arrayOfPhoneLinks = [];
     $(arr).each((i, elem) => {
-        let n = ($(elem).find('.g-i-list-title').html());
-
-        if (n !== null) {
-            arrayOfPhoneLinks.push(n.match(/href="([^"]*)/)[1]);
-        }
+        let n = $(elem).find('.g-i-list-title').html();
+        if (n !== null) arrayOfPhoneLinks.push(n.match(/href="([^"]*)/)[1]);
     });
 
     let promises = [];
-
+    // Build URL for characteristics tab and call method who parse it
     arrayOfPhoneLinks.forEach(phoneIrl => {
-        phoneIrl = phoneIrl + '#tab=characteristics';
-        promises.push(getPhoneInfo(phoneIrl));
+        promises.push(getPhoneInfo(phoneIrl + '#tab=characteristics'));
     });
 
     let characteristicPhones = await Promise.all(promises);
-
+    // Log in console all phones characteristic (response.json analog)
     console.log(characteristicPhones);
-
 }
 
-const getPhoneInfo = async (url) => {
-    let page = await request(url);
+/**
+ * @param onePhoneURL - URL
+ * @returns {Promise<{nameOfPhone: string, techChar: Array}>}
+ * This method parse HTML file of phone. Take params from it, build response object and return it.
+ */
+const getPhoneInfo = async (onePhoneURL) => {
+    let page = await request(onePhoneURL);
     let $ = cheerio.load(page);
     // Get HTML of class what contains name of phone and trim it to get just model
     let [nameOfPhone] = $('.detail-title').html().trim().split(' + &#');
 
     let infoAboutPhone = {nameOfPhone, techChar: []};
-
+    // parse HTML page and find params and build response object
     $('.chars-t-cell').each((index, elem) => {
-        let descr = $(elem).text().trim();
-        let characteristic = {};
-        if (index % 2 === 0) characteristic.description = descr;
-        else characteristic.value = descr;
+        let parsedParam = $(elem).text().trim();
+        let phoneCharacteristic = {};
+        // If the index is even its a description of phone property. If odd its value of the property
+        if (index % 2 === 0) phoneCharacteristic.description = parsedParam;
+        else phoneCharacteristic.value = parsedParam;
 
-        infoAboutPhone.techChar.push(characteristic);
+        infoAboutPhone.techChar.push(phoneCharacteristic);
     });
-    // console.log(infoAboutPhone);
     return infoAboutPhone;
 };
 
